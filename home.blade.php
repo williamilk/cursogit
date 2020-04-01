db963fe7fbd222b57d992133a499ac0b4e908de0
william-leite <williamilk@hotmail.com>
2020-03-31 21:33:25 -0300

    add 2 arquivos

---
+++ home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+	
+    <div class="container">
+		<div class="row">
+			<div class="col-md-12">
+                <div class="title">
+				<center>
+				   <p> <h1>Seja bem-vindo!</h1></p>
+			       <p><h2>Utilize o menu acima para listar, cadastrar, alterar e deletar dados. <br>
+				   
+				   </h2></p>
+                </center>
+				</div>
+            </div>
+			</div>
+        </div>
+@endsection
